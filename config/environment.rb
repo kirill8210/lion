eda3232f70335755333b6554c2cbf0b18ab95a35
Kirill <kirill.zp@mail.ru>
2015-01-22 12:20:26 +0200

    Finish layout and routes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {  
+
+  :address => "smtp.gmail.com",
+  :port => "587",
+  :domain => "baci.lindsaar.net",
+  :enable_starttls_auto => true,
+  :authentication => :login,
+  :user_name => "kyryl.zp@gmail.com",
+  :password => "zp0401aa"
+
+}
