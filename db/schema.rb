2962275b14d914de782b5a3fce4ef4c7f22ae48f
Kirill <kirill.zp@mail.ru>
2015-01-22 16:05:41 +0200

    Finish sign in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150122113541) do
+ActiveRecord::Schema.define(version: 20150122130409) do
 
   create_table "users", force: true do |t|
     t.string   "name"
